72098bcc5b1dc3064157596ba36cf5fda2d4f0a5
switzerit <contact.switzerit@gmail.com>
2026-05-26 15:21:19 +0000

    fix: testimonials blanc, advice gris

--- src/component/Home/Advice.jsx
+++ src/component/Home/Advice.jsx
@@ -18,7 +18,7 @@ export default function CTASection() {
 
 
     return (
-        <div className="w-full pt-12 pb-20 bg-white border-t-2 border-gray-200">
+        <div className="w-full pt-12 pb-20 bg-gray-50 border-t-2 border-gray-200">
             <div className="max-w-7xl mx-auto text-center px-3">
                 {/* Heading */}
                 <motion.h2
