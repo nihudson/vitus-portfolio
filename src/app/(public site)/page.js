d20acc2d842939fc136d12294933783addee5160
switzerit <contact.switzerit@gmail.com>
2026-05-26 20:19:54 +0000

    feat: testimonials avec titre + bandeau défilant

--- src/app/(public site)/page.js
+++ src/app/(public site)/page.js
@@ -3,15 +3,11 @@ import Hero from "@/component/Home/Hero";
 import Services from "@/component/Home/Services";
 import WhyChooseSwitzerIT from "@/component/Home/Whychoose";
 import Testimonials from "@/component/Home/Testimonials";
-import TestimonialsMarquee from "@/component/Home/TestimonialsMarquee";
 
 export default function page() {
   return (
     <>
       <Hero />
-      <div style={{ marginTop: "-150px", position: "relative", zIndex: 20 }}>
-        <TestimonialsMarquee />
-      </div>
       <Services />
       <WhyChooseSwitzerIT />
       <Testimonials />
